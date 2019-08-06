2084df1ec53a1dac804599be612608c9c122449a
admin <zatravkin.kir@gmail.com>
2019-08-06 23:28:12 +0300

    1

--- src/main/java/CSV.java
+++ src/main/java/CSV.java
@@ -4,8 +4,8 @@ import org.supercsv.io.CsvBeanWriter;
 import org.supercsv.io.ICsvBeanWriter;
 import org.supercsv.prefs.CsvPreference;
 
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.StringWriter;
 import java.util.ArrayList;
 
 //create CSV file
@@ -34,15 +34,15 @@ public class CSV {
 
 
     public void createCSVFile(ArrayList<ProductModel> productModels) throws IOException {
-        StringWriter writer = new StringWriter();
-        ICsvBeanWriter csvBeanWriter = new CsvBeanWriter(writer, CsvPreference.STANDARD_PREFERENCE);
+        ICsvBeanWriter csvBeanWriter = null;
+        csvBeanWriter = new CsvBeanWriter(new FileWriter("target/1.csv"), CsvPreference.STANDARD_PREFERENCE);
         String[] header = new String[]{"productId", "productImage", "minPrice", "maxPrice", "oriMinPrice", "oriMaxPrice", "discount", "startTime", "endTime", "orders", "shopUrl", "productTitle","sellerId","productDetailUrl"};
         csvBeanWriter.writeHeader(header);
         for (ProductModel product : productModels) {
             csvBeanWriter.write(product, header, getProcessors());
         }
         csvBeanWriter.close();
-        System.out.println(writer.toString());
+
     }
 }
 
